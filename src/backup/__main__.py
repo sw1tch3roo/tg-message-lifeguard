ee619ca0b5602dce0475b766865805ce17d54f1a
MXLV69 <markplusgood@gmail.com>
2025-09-07 04:15:06 +0400

    Removed app_version param from line 32 because it triggered an error: TelegramBaseClient.__init__() takes 4 position>al arguments but 5 were given

--- src/backup/__main__.py
+++ src/backup/__main__.py
@@ -29,7 +29,7 @@ if os.path.exists(session_file):
     print(f"Existing session file removed: {session_file}")
 
 # Initialize Telegram client
-client: TelegramClient = TelegramClient(session_name, api_id, api_hash, app_version)
+client: TelegramClient = TelegramClient(session_name, api_id, api_hash)
 
 
 async def export_messages(
